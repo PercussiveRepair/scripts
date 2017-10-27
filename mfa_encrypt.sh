e1201116ff3c958e90b3d3dce11718809399aded
PercussiveRepair <jay.harrison@bgch.co.uk>
2017-10-27 16:15:01 +0100

    updated

--- mfa_encrypt.sh
+++ mfa_encrypt.sh
@@ -5,12 +5,12 @@
 MFALOCATION="/Users/jayharrison/Desktop/mfa"
 SECRETSREPO="/Users/jayharrison/repos/ops-secrets"
 
-cp $MFALOCATION/*.png $SECRETSREPO/passwords/root_mfa_qr/
+mv $MFALOCATION/*.png $SECRETSREPO/passwords/root_mfa_qr/
 for i in $(ls $SECRETSREPO/passwords/root_mfa_qr/*.png); do
   echo "encrypting $i"
   gpg  $(cat $SECRETSREPO/ops.recipients | awk '{print "-r", $1}' | xargs) --always-trust --encrypt $i
   if [ -e $i.gpg ]; then
     echo "encrypted $(ls $i) to $(ls $i.gpg)"
-    rm $i.gpg
+    rm $i
   fi
 done
