c18019038c45c281ee096a7c485893156a586c7a
PercussiveRepair <jay.harrison@bgch.co.uk>
2018-01-09 16:15:55 +0000

    adding tags

--- shortids.sh
+++ shortids.sh
@@ -3,16 +3,20 @@
 # obtains all the instances in all the regions in all the accounts that have old (short) ids
 
 profiles=$(grep -oE '\[.*?\]' ~/.aws/credentials | grep -Ev 'default|awsbillingmaster' | tr -d '[]' | sort)
+#profiles="connectedhome-dev"
 regions=$(aws ec2 describe-regions --output text | cut -f 3)
 
-echo "account, region, instanceid"
+echo "account,region,instanceid,tags"
 for profile in $profiles; do 
   for region in $regions; do
-    ids=$(aws --region=$region --profile=$profile ec2 describe-instances | jq -r '.Reservations[].Instances[].InstanceId')
-    for id in $ids; do
+    idsandtags=$(aws --region=$region --profile=$profile ec2 describe-instances | jq -r '.Reservations[].Instances[] |[ .InstanceId, (.Tags[]? | .Value)] | @csv')
+    IFS=$'\n' 
+    for idandtag in $idsandtags; do
+      ec2info=$(echo $idandtag | tr -d \")
+      id=$(echo $ec2info | cut -f 1 -d ",")
       LEN=$(echo ${#id})
       if [ $LEN -lt 11 ]; then
-        echo "$profile, $region, $id"
+        echo "$profile,$region,$ec2info"
       fi
     done
   done
